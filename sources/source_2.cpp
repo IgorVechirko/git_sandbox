8b8e9ac0832631ea627726314fb7b4461fb3fe32
Igor <vechirkoigor@gmail.com>
2023-12-01 14:08:26 +0200

    add cpp func

--- sources/source_2.cpp
+++ sources/source_2.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void isElemExist(const std::vector<int>& _array, int _elem)
+{
+	return std::find(_array.cbegin(), _array.cend(), _elem) != _array.cend();
+}
+
 int main(int argc, char** argv)
 {
 	return 0;
